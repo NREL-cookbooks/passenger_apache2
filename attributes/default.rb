a1c33b0b0b26347824ec2042d53d86e0a70d03b8
Nick Muerdter <nick.muerdter@nrel.gov>
2013-04-09 20:59:10 -0600

    Make more of the passenger variables configurable.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,46 @@ default['passenger']['version']     = '3.0.19'
 default['passenger']['apache_mpm']  = nil
 default['passenger']['root_path']   = "#{languages['ruby']['gems_dir']}/gems/passenger-#{passenger['version']}"
 default['passenger']['module_path'] = "#{passenger['root_path']}/ext/apache2/mod_passenger.so"
-default['passenger']['max_pool_size'] = 6
 default['passenger']['manage_module_conf'] = true
 default['passenger']['package']['name'] = nil
 default['passenger']['package']['version'] = node['passenger']['version']
+
+# General
+default[:passenger][:spawn_method] = "smart-lv2"
+default[:passenger][:use_global_queue] = true
+
+# Security
+default[:passenger][:user_switching] = true
+default[:passenger][:default_user] = "nobody"
+default[:passenger][:default_group] = nil
+default[:passenger][:friendly_error_pages] = true
+
+# Resource control and optimization
+default[:passenger][:max_pool_size] = 6
+default[:passenger][:min_instances] = 1
+default[:passenger][:max_instances_per_app] = 0
+default[:passenger][:pool_idle_time] = 300
+default[:passenger][:max_requests] = 0
+default[:passenger][:stat_throttle_rate] = 0
+default[:passenger][:pre_start_urls] = []
+default[:passenger][:high_performance] = false
+
+# Compatibility
+default[:passenger][:resolve_symlinks_in_document_root] = false
+default[:passenger][:allow_encoded_slashes] = false
+
+# Logging and debugging
+default[:passenger][:log_level] = 0
+default[:passenger][:debug_log_file] = nil
+
+# Ruby on Rails-specific
+default[:passenger][:rails_auto_detect] = true
+default[:passenger][:rails_base_uri] = nil
+default[:passenger][:rails_env] = "production"
+default[:passenger][:rails_framework_spawner_idle_time] = 1800
+default[:passenger][:rails_app_spawner_idle_time] = 600
+
+# Rack-specific
+default[:passenger][:rack_auto_detect] = true
+default[:passenger][:rack_base_uri] = nil
+default[:passenger][:rack_env] = "production"
