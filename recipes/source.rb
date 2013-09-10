b2e5bebc4ded4471561141e477097555d0173d6a
Simon Robson <shrobson@gmail.com>
2013-09-09 23:14:23 -0400

    [COOK-3395] Make sure to compile the version of passenger on the node attrribute
    Signed-off-by: Seth Vargo <sethvargo@gmail.com>

--- recipes/source.rb
+++ recipes/source.rb
@@ -52,6 +52,6 @@ rbenv_gem "passenger" do
 end
 
 execute "passenger_module" do
-  command 'passenger-install-apache2-module --auto'
+  command "passenger-install-apache2-module _#{node['passenger']['version']}_ --auto"
   creates "#{node['passenger']['root_path']}/#{node['passenger']['module_path']}"
 end
